b573b78d2a81ed1280ab5ea1571481364914c79f
EvaBoxler <e.a.boksler@gmail.com>
2022-07-20 00:13:57 +0700

    src

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: '/src/index.js',
+  output: {
+    filename: 'index.js',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+};
+
+export default () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
